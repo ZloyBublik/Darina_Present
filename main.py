69023e40f4319cee16914c00172eb99568c681ea
mikhailshalashov <chessstufff@gmail.com>
2025-12-13 11:57:01 +0100

    All new stuff: 1. Added who said it game 2. Changed todays msg 3. Added new page

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def home():
     else:
         return render_template("index.html")
 
+@app.route("/whosaidit")
+def whosaidit():
+    return render_template("whosaidit.html")
+
 if __name__ == "__main__":
     #port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
     #app.run(host="0.0.0.0", port=port, debug=True)
